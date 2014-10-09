965b4c0d5c6548394b68d69507cd1094650acc20
pohh-mell <meelissulg1@gmail.com>
2014-10-09 19:41:31 +0300

    HeiHo J-Ho

--- src/ee/ut/math/tvt/team7/IntroUI.java
+++ src/ee/ut/math/tvt/team7/IntroUI.java
@@ -1,20 +1,31 @@
 package ee.ut.math.tvt.team7;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class IntroUI extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
-		Stage lava = new Stage();
-		BorderPane piir = new BorderPane();
-		Scene stseen = new Scene(piir, 500, 600, Color.SNOW);
-		lava.setScene(stseen);
-		lava.show();
+		Group lava = new Group();
+        Scene stseen = new Scene(lava, 600, 500, Color.SNOW);
+        Text nimi =new Text("Team 7");
+        nimi.setLayoutX(20);
+        nimi.setLayoutY(20);
+        lava.getChildren().add(nimi);
+        Text liider =new Text("Lauri OUT");
+        liider.setLayoutX(20);
+        liider.setLayoutY(80);
+        lava.getChildren().add(liider);
+        primaryStage.setScene(stseen);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
