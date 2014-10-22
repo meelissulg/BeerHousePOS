528a442de110cb2cd6cf2ab741dbde097f820830
pohh-mell <meelissulg1@gmail.com>
2014-10-22 10:57:26 +0300

    intro koos erroritega

--- src/ee/ut/math/tvt/team7/Intro.java
+++ src/ee/ut/math/tvt/team7/Intro.java
@@ -8,6 +8,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
+import ee.ut.math.tvt.salessystem.domain.controller.SalesDomainController;
+import ee.ut.math.tvt.salessystem.domain.controller.impl.SalesDomainControllerImpl;
+import ee.ut.math.tvt.salessystem.ui.ConsoleUI;
+import ee.ut.math.tvt.salessystem.ui.SalesSystemUI;
 import ee.ut.math.tvt.team7.IntroUI;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -15,6 +21,9 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Intro extends Application {
+
+private static final Logger log = Logger.getLogger(Intro.class);
+private static final String MODE = "console";
 	public static String tiim;
 	public static String liider;
 	public static String liikmed;
@@ -33,6 +42,34 @@ public class Intro extends Application {
 	}
 
 	public static void main(String[] args) {
+		
+		final SalesDomainController domainController = new SalesDomainControllerImpl();
+
+		if (args.length == 1 && args[0].equals(MODE)) {
+			log.debug("Mode: " + MODE);
+
+			ConsoleUI cui = new ConsoleUI(domainController);
+			cui.run();
+		} else {
+
+			IntroUI introUI = new IntroUI();
+			introUI.setVisible(true);
+			introUI.setAlwaysOnTop(true);
+
+			final SalesSystemUI ui = new SalesSystemUI(domainController);
+			ui.setVisible(true);
+
+			introUI.setAlwaysOnTop(false);
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			introUI.setVisible(false);
+		}
+	
+	
+		
 		
 		Properties prop = new Properties();
 		
