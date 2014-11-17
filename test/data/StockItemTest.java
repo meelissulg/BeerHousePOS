ca9256e1474ddced297bb6c86b49c9aed4f43c16
keilu <keili.pedel@gmail.com>
2014-11-17 11:15:39 +0200

    lauriviin

--- test/data/StockItemTest.java
+++ test/data/StockItemTest.java
@@ -1,5 +1,19 @@
 package data;
 
-public class StockItemTest {
+import ee.ut.math.tvt.salessystem.domain.data.StockItem;
 
+public class StockItemTest {
+	StockItem stockItem;
+	
+	public void setUp() {
+		stockItem = new StockItem((long) 888, "Lauriviin", "test", 5.5, 3);
+	}
+	
+	public void testClone(){
+		
+	}
+	
+	public void testGetColumn(){
+		
+	}
 }
